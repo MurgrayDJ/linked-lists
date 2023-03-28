e21f3aa63b95682ae95aa535e845765f414f0c1f
murgraydj <murgraydj@gmail.com>
2023-03-27 20:08:23 -0400

    Creates initialize method for Node class

--- linked_list.rb
+++ linked_list.rb
@@ -4,10 +4,12 @@
 
 class LinkedList
   class Node
-    @next_node = nil
-    def value 
-      nil
+    attr_reader :value
+    attr_accessor :next_node
+
+    def initialize(value=nil, next_node=nil)
+      @value = value
+      @next_node = next_node
     end
   end
-
 end
