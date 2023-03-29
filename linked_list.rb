ffecae5b3f2e6899a1cd692afbcbd8d1084339c0
murgraydj <murgraydj@gmail.com>
2023-03-29 13:54:58 -0400

    Adds pop method to LinkedList class
    This method removes the last item of a linked list.

--- linked_list.rb
+++ linked_list.rb
@@ -40,23 +40,6 @@ class LinkedList
     end
   end
 
-  def at(index)
-    current_index = 0
-    value = nil
-    current_node = @head
-
-    until current_node.nil?
-      if current_index == index
-        value = current_node.value
-        break
-      else
-        current_index += 1
-        current_node = current_node.next_node
-      end
-    end
-    value
-  end
-
   def size
     size = 0
     current_node = @head
@@ -75,9 +58,46 @@ class LinkedList
     @tail
   end
 
+
+  def at(index)
+    current_index = 0
+    value = nil
+    current_node = @head
+
+    until current_node.nil?
+      if current_index == index
+        value = current_node.value
+        break
+      else
+        current_index += 1
+        current_node = current_node.next_node
+      end
+    end
+    value
+  end
+
+  def pop
+    current_node = @head
+    popped_node = nil
+    if @head.nil?
+      puts "This list is empty."
+    elsif @head == @tail
+      popped_node = @head
+      @head = @tail = nil
+    else
+      until current_node.next_node == @tail
+        current_node = current_node.next_node
+      end
+      popped_node = current_node.next_node
+      current_node.next_node = nil
+      @tail = current_node
+    end
+    popped_node
+  end
+
   def to_s
     list_as_string = ""
-    if head.nil?
+    if @head.nil?
       list_as_string = "This list is empty."
     else
       current_node = @head
@@ -92,21 +112,26 @@ class LinkedList
   list = LinkedList.new()
 
   list.append(3)
-  puts "Current head: #{list.head.value}"
-  puts "Current size: #{list.size}"
+  # puts "Current head: #{list.head.value}"
+  # puts "Current size: #{list.size}"
   puts list.to_s
 
   list.append(4)
-  puts "Node after head: #{list.head.next_node.value}"
-
-  list.append(5)
-  puts "Current tail: #{list.tail.value}"  
-  puts "Current size: #{list.size}"
-  puts "Value at index 2 is: #{list.at(2)}"
+  # puts "Node after head: #{list.head.next_node.value}"
   puts list.to_s
 
+  list.append(5)
+  # puts "Current tail: #{list.tail.value}"  
+  # puts "Current size: #{list.size}"
+  # puts "Value at index 2 is: #{list.at(2)}"
+  puts list.to_s
+  puts "Node popped off: ( #{list.pop.value} )"
+
   list.prepend(1)
-  puts "Current head: #{list.head.value}"
-  puts "Current size: #{list.size}"
+  # puts "Current head: #{list.head.value}"
+  # puts "Current size: #{list.size}"
+  puts list.to_s
+
+  
   puts list.to_s
 end
